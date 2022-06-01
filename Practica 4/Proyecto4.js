1c5b253c5a4efe1e286c26ca8a9831e9bd09afba
faksu <dk>
2022-06-01 12:31:18 -0300

    proyecto4terminado

--- Practica 4/Proyecto4.js
+++ Practica 4/Proyecto4.js
@@ -1,9 +1,10 @@
 var mostrarImagenes = "por def";
 
 function mostrar(){
-    document.querySelector("#cantidad_imagenes").value;
+    mostrarImagenes = document.querySelector("#cantidad_imagenes").value;
     for (let i = 0; i <= mostrarImagenes; i++){
-        document.querySelector("#resultado"). innerHTML += 
-        document.write ("<img src='https://www.pngitem.com/pimgs/m/653-6531476_chibi-solaire-praise-the-sun-hd-png-download.png' alt='Praise the sun'")
+        document.querySelector("#resultado").innerHTML += `
+        <img src='https://www.pngitem.com/pimgs/m/653-6531476_chibi-solaire-praise-the-sun-hd-png-download.png' alt='Praise the sun'>")
+        `;
     }
 }
