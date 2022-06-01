e6a07038c6a8579307508843c19fad0a1add6350
faksu <dk>
2022-06-01 13:36:22 -0300

    parte5

---
+++ Practica 5/Proyecto5.js
@@ -0,0 +1,10 @@
+var cantidadOpciones = "por def";
+
+function ampliar(){
+    cantidadOpciones = document.querySelector("#cantidad_ingresada").value;
+    for (let i = 1; i <= cantidadOpciones; i++){
+        document.querySelector("#cantidad_seleccionar").innerHTML+=`
+         Opcion: ${i}
+        `;
+    }
+}
