33e61c438db4b915734b45cc41ca18188af2cee4
faksu <dk>
2022-06-01 14:50:21 -0300

    practicaforterminada

--- Practica_5/Proyecto5.js
+++ Practica_5/Proyecto5.js
@@ -1,9 +1,14 @@
 var cantidadOpciones = "por def";
 
+
 function ampliar(){
-    cantidadOpciones = document.querySelector("#cantidad_ingresada").value;
+    cantidadOpciones = document.querySelector("#cantidad_ingresada").value; 
+    document.querySelector("#cantidad_seleccionada").innerHTML=`
+    <option>Seleccione...</option>
+    `;
     for (let i = 1; i <= cantidadOpciones; i++){
-        document.querySelector("#cantidad_seleccionar").innerHTML+=`
-         <div> Opcion: ${i} </div>`;
+        document.querySelector("#cantidad_seleccionada").innerHTML+=`
+         <option>Opcion: ${i}</option>
+         `;
     }
 }
